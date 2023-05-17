b50fd0897218d689fb7a7c42776165ef79d3db6c
adelineg03 <guedin.adeline03@gmail.com>
2023-05-17 12:01:37 +0200

    INIT PROJECT

---
+++ portfolio/src/app/COMPONENTS/skill-cards/skill-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-cards',
+  templateUrl: './skill-cards.component.html',
+  styleUrls: ['./skill-cards.component.scss']
+})
+export class SkillCardsComponent {
+
+}
---
+++ portfolio/src/app/COMPONENTS/xp-cards/xp-cards.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xp-cards',
+  templateUrl: './xp-cards.component.html',
+  styleUrls: ['./xp-cards.component.scss']
+})
+export class XpCardsComponent {
+
+}
