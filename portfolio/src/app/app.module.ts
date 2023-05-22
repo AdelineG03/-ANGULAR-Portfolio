3b9fa200b6928d57b703c9789b2f057faafe6838
adelineg03 <guedin.adeline03@gmail.com>
2023-05-22 09:34:01 +0200

    init

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BurgerMenuComponent } from './COMPONENTS/burger-menu/burger-menu.component';
+import { DividerComponent } from './COMPONENTS/divider/divider.component';
+import { FooterComponent } from './COMPONENTS/footer/footer.component';
+import { ProfilComponent } from './COMPONENTS/profil/profil.component';
+import { ProjectCardsComponent } from './COMPONENTS/project-cards/project-cards.component';
+import { SkillCardsComponent } from './COMPONENTS/skill-cards/skill-cards.component';
+import { XpCardsComponent } from './COMPONENTS/xp-cards/xp-cards.component';
+import { NavBarComponent } from './COMPONENTS/nav-bar/nav-bar.component';
+import { PortfolioComponent } from './COMPONENTS/portfolio/portfolio.component';
+import { ExperiencesComponent } from './COMPONENTS/experiences/experiences.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BurgerMenuComponent,
+    DividerComponent,
+    FooterComponent,
+    ProfilComponent,
+    ProjectCardsComponent,
+    SkillCardsComponent,
+    XpCardsComponent,
+    NavBarComponent,
+    PortfolioComponent,
+    ExperiencesComponent,
   ],
   imports: [
     BrowserModule,
